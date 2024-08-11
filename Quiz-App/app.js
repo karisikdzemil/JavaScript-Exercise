c1d113c775928442bad3fc13bfb323bfdcf524d6
dzemil karisik <karisikdzemil@gmail.com>
2024-08-11 17:16:47 +0200

    rendereing objects

--- Quiz-App/app.js
+++ Quiz-App/app.js
@@ -2,58 +2,75 @@ const quiz = [
     {
     question: "What is the capital of France?",
     cAnswer: "Paris",
-    wAnswers: ["Berlin", "Madrid", "Rome"]
+    wAnswers: ["Berlin", "Paris", "Madrid", "Rome"]
 },
 {
     question: "Which planet is known as the Red Planet?",
     cAnswer: "Mars",
-    wAnswers: ["Venus", "Jupiter", "Saturn"]
+    wAnswers: ["Venus", "Jupiter", "Saturn", "Mars"]
 },
 {
     question: "Who wrote the play Romeo and Juliet?",
     cAnswer: "William Shakespeare",
-    wAnswers: ["Charles Dickens", "Mark Twain", "Jane Austen"]
+    wAnswers: ["William Shakespeare", "Charles Dickens", "Mark Twain", "Jane Austen"]
 },
 {
     question: "What is the largest mammal in the world?",
     cAnswer: "Blue Whale",
-    wAnswers: ["Elephand", "Giraffe", "Polar Bear"]
+    wAnswers: ["Elephand", "Giraffe", "Blue Whale", "Polar Bear"]
 },
 {
     question: "Which element has the chemical symbol 'O'?",
     cAnswer: "Oxygen",
-    wAnswers: ["Hydrogen", "Iron", "Gold"]
+    wAnswers: ["Hydrogen", "Oxygen", "Iron", "Gold"]
 },
 {
     question: "How many continents are there on Earth?",
     cAnswer: "7",
-    wAnswers: ["5", "6", "8"]
+    wAnswers: ["5", "6", "7", "8"]
 },
 {
     question: "Which ocean is the largest?",
     cAnswer: "Pacific Ocean",
-    wAnswers: ["Atlantic Ocean", "Indian Ocean", "Arctic Ocean"]
+    wAnswers: ["Atlantic Ocean", "Indian Ocean", "Arctic Ocean", "Pacific Ocean"]
 },
 {
     question: "What is the smallest prime number?",
     cAnswer: "2",
-    wAnswers: ["0", "1", "3"]
+    wAnswers: ["0", "1", "2", "3"]
 },
 {
     question: "Who was the first President of the United States?",
     cAnswer: "George Washington",
-    wAnswers: ["Abraham Lincoln", "Thomas Jefferson", "John Adams"]
+    wAnswers: ["George Washington", "Abraham Lincoln", "Thomas Jefferson", "John Adams"]
 },
 {
     question: "What is the chemical formula for water?",
     cAnswer: "H2O",
-    wAnswers: ["CO2", "NaCl", "O2"]
+    wAnswers: ["CO2","H2O", "NaCl", "O2"]
 },
 ];
 
-for(const que of quiz){
-    for(obj in que){
-        console.log(que[obj]);
+// for(const que of quiz){
+//     for(obj in que){
+//         console.log(que[obj]);
+//     }
+//     console.log("------------------")
+// }
+
+const question = document.querySelector("h2");
+const answers = document.querySelectorAll("li");
+const nextQuestion = document.getElementById("next");
+
+
+
+nextQuestion.addEventListener("click", ()=>{
+    let randomIndex = (Math.random() * 10).toFixed(0);
+    console.log(randomIndex)
+    question.textContent = quiz[randomIndex].question;  
+    for(let i = 0;i<4;i++){
+        
+        answers[i].textContent = quiz[randomIndex].wAnswers[i];
     }
-    console.log("------------------")
-}
+})
+
