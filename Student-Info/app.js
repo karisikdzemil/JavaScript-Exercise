7de3bddc799e0a46f78af72a70a7eff97591e894
dzemil karisik <karisikdzemil@gmail.com>
2024-08-20 18:57:37 +0200

    80% of style and simple js logic

--- Student-Info/app.js
+++ Student-Info/app.js
@@ -1,2 +1,4 @@
-  console.log(students);
-  
+const ul = document.querySelector("ul");
+students.map((el)=>{
+    ul.innerHTML = `<li>${el.name} ${el.surname} -- ${el.index} -- ${el.department}</li>`;
+})  
