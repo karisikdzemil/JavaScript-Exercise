953a9e1267995763ca3ae0a7ed9517b095727ebc
dzemil karisik <karisikdzemil@gmail.com>
2024-08-21 15:04:58 +0200

    search student logic

--- Student-Info/app.js
+++ Student-Info/app.js
@@ -1,4 +1,29 @@
 const ul = document.querySelector("ul");
-students.map((el)=>{
-    ul.innerHTML = `<li>${el.name} ${el.surname} -- ${el.index} -- ${el.department}</li>`;
-})  
+const input = document.querySelector("input");
+const searchBtn = document.getElementById("input-btn-search");
+
+const renderItems = (renderArray) => {
+  renderArray.map((element) => {
+    const li = document.createElement("li");
+    li.textContent = `${element.name} ${element.surname} -- Indeks: ${element.department}`;
+    ul.append(li);
+  });
+};
+renderItems(students);
+
+searchBtn.addEventListener("click", () => {
+  const searchValue = input.value.toUpperCase().replace(/\s+/g, "");
+  console.log(searchValue);
+  const filteredArr = students.filter((el) => {
+    if (
+      searchValue === el.name.toUpperCase() ||
+      searchValue === el.surname.toUpperCase() ||
+      searchValue === el.name.toUpperCase() + el.surname.toUpperCase() ||
+      searchValue === el.department.toUpperCase()
+    ) {
+      return el;
+    }
+  });
+  ul.innerHTML = "";
+  renderItems(filteredArr);
+});
