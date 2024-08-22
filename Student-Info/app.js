f3b82e74e46f22b9d69023c463aebf225e701c2d
dzemil karisik <karisikdzemil@gmail.com>
2024-08-22 16:55:25 +0200

    working on more functionality

--- Student-Info/app.js
+++ Student-Info/app.js
@@ -9,6 +9,7 @@ const addStudentBtn = document.getElementById("add-student-btn");
 const addStudentModal = document.getElementById("add-student");
 const inputs = addStudentModal.querySelectorAll("input");
 const showStudentModal = document.getElementById("show-student");
+const closeShowBtn = document.getElementById("close-show-modal");
 
 const renderItems = (renderArray = students) => {
   ul.innerHTML = "";
@@ -16,8 +17,8 @@ const renderItems = (renderArray = students) => {
     const li = document.createElement("li");
     const left = document.createElement("span");
     const right = document.createElement("span");
-    left.textContent = `${element.name} ${element.surname} `
-    right.textContent = ` ${element.department}`;
+    left.textContent = `${element.name} ${element.surname}`;
+    right.textContent = ` ${element.index}`;
     li.append(left, right);
     ul.append(li);
   });
@@ -40,50 +41,78 @@ const searchStudentHandler = () => {
   if (filteredArr.length > 0) {
     ul.innerHTML = "";
     renderItems(filteredArr);
-    input.value = '';
+    input.value = "";
   } else {
     alert("Student not found!!");
-    input.value = '';
+    input.value = "";
   }
 };
 
-const addStudentHandler = () =>{
+const addStudentHandler = () => {
   const studentCourses = [...inputs[4].value.split(",")];
   const studentGrades = [...inputs[3].value.split(",")];
-  const coursesAndGrades = {}
-  studentCourses.forEach((el, index)=>{
+  const coursesAndGrades = {};
+  studentCourses.forEach((el, index) => {
     coursesAndGrades[el] = studentGrades[index];
-  })
+  });
   const newStudent = {
     name: inputs[0].value,
     surname: inputs[1].value,
-    index: inputs[2].value, 
-    grades: {...coursesAndGrades},
+    index: inputs[2].value,
+    grades: { ...coursesAndGrades },
     courses: studentCourses,
-    department: inputs[5].value
-  }
-    students.push(newStudent);
-    renderItems(students);
-    inputs.forEach(el=> el.value = '');
-    blackDrop.classList.toggle("visible");
-    addStudent.classList.toggle("visible");
-}
+    department: inputs[5].value,
+  };
+  students.push(newStudent);
+  renderItems(students);
+  inputs.forEach((el) => (el.value = ""));
+  blackDrop.classList.toggle("visible");
+  addStudent.classList.toggle("visible");
+};
 
-const showStudentHandler = (event) =>{
+const showStudentHandler = (event) => {
+  const showName = showStudentModal.children[1];
+  const showIndex = showStudentModal.children[2];
+  const showGrades = showStudentModal.children[3];
+  const showCourses = showStudentModal.children[4];
+  const showDepartment = showStudentModal.children[5];
+  const showAverage = showStudentModal.children[6];
+  // console.log(showName, showIndex, showGrades, showCourses, showDepartment, showAverage);
 
-  if(event.target.tagName !== "UL"){
-    console.log(event.target.textContent); 
+  let clickedElement = event.target;
+
+  if (clickedElement.tagName === "SPAN") {
+    clickedElement = clickedElement.closest("li");
   }
-}
+  if (clickedElement.tagName === "LI") {
+    const indexOfEl = clickedElement.children[1].textContent;
+
+    const showStudent = students.find(
+      (el) => indexOfEl.trim() === el.index.trim()
+    );
+    let entries = Object.entries(showStudent.grades).map(([key, value])=> `${key}: ${value}`);
+
+    showName.textContent = showStudent.name + " " + showStudent.surname;
+    showIndex.textContent = showStudent.index;
+    showGrades.textContent = entries.join(" ");
+    showCourses.textContent = `${showStudent.courses[0]}, ${showStudent.courses[1]}, ${showStudent.courses[2]}`;
+    showDepartment.textContent = showStudent.department;
+    // showIndex.textContent = ;
+
+    console.log(showStudent);
+    console.log(indexOfEl);
+  }
+};
 
 searchBtn.addEventListener("click", searchStudentHandler);
 addStudentBtn.addEventListener("click", addStudentHandler);
-addBtn.addEventListener("click", ()=>{
-    blackDrop.classList.toggle("visible");
-    addStudent.classList.toggle("visible");
-})
+addBtn.addEventListener("click", () => {
+  blackDrop.classList.toggle("visible");
+  addStudent.classList.toggle("visible");
+});
 h1.addEventListener("click", () => {
   renderItems(students);
 });
+closeShowBtn.addEventListener("click", showStudentHandler);
 
 ul.addEventListener("click", showStudentHandler);
