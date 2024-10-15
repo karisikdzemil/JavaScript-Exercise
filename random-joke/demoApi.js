0435b67e577b7262d4c6cb9ce324f3db33c54729
dzemil karisik <karisikdzemil@gmail.com>
2024-10-15 18:38:14 +0200

    create new project folder

---
+++ random-joke/demoApi.js
@@ -0,0 +1,51 @@
+const jokes = [
+  {
+    id: 1,
+    joke: "Why don’t skeletons fight each other? They don’t have the guts.",
+  },
+  { id: 2, joke: "What do you call cheese that isn’t yours? Nacho cheese." },
+  { id: 3, joke: "Why did the bicycle fall over? It was two-tired!" },
+  { id: 4, joke: "Why don’t eggs tell jokes? They’d crack each other up." },
+  { id: 5, joke: "I used to play piano by ear, but now I use my hands." },
+  {
+    id: 6,
+    joke: "Why can’t you hear a pterodactyl go to the bathroom? Because the 'P' is silent.",
+  },
+  { id: 7, joke: "Why did the math book look sad? It had too many problems." },
+  { id: 8, joke: "What did the big flower say to the little flower? Hi, bud!" },
+  {
+    id: 9,
+    joke: "I’m reading a book on anti-gravity. It’s impossible to put down!",
+  },
+  {
+    id: 10,
+    joke: "Why don’t seagulls fly over the bay? Because then they’d be bay-gulls.",
+  },
+  {
+    id: 11,
+    joke: "Why did the scarecrow win an award? Because he was outstanding in his field!",
+  },
+  { id: 12, joke: "How do you organize a space party? You planet." },
+  { id: 13, joke: "I’m on a seafood diet. I see food and I eat it." },
+  { id: 14, joke: "What’s orange and sounds like a parrot? A carrot." },
+  {
+    id: 15,
+    joke: "I told my wife she was drawing her eyebrows too high. She looked surprised.",
+  },
+  { id: 16, joke: "How does a penguin build its house? Igloos it together." },
+  { id: 17, joke: "What do you call fake spaghetti? An impasta." },
+  {
+    id: 18,
+    joke: "Why are ghosts bad at lying? Because they are too transparent.",
+  },
+  {
+    id: 19,
+    joke: "Why did the golfer bring extra pants? In case he got a hole in one.",
+  },
+  {
+    id: 20,
+    joke: "What do you get when you cross a snowman and a vampire? Frostbite.",
+  },
+];
+
+console.log(jokes);
