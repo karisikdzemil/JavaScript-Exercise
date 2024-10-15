0dc745f3b48a4226f49a96920f30cc9e83aaa5ca
dzemil karisik <karisikdzemil@gmail.com>
2024-10-15 19:57:24 +0200

    style, and some logic

--- random-joke/app.js
+++ random-joke/app.js
@@ -1,1 +1,18 @@
-console.log("radi")
+const btn = document.getElementById("btn");
+const p = document.getElementById("paragraph");
+
+const randomJoke = () =>{
+    const num1 = (Math.random() * 20).toFixed(0);
+    const num2 = (Math.random() * 20).toFixed(0);
+    const findEl = jokes.find((el)=>{
+        return el.id == num1;
+    })
+    // return findEl;
+    console.log(num1, findEl)
+}
+
+const joke = () =>{
+    randomJoke()
+}
+
+btn.addEventListener("click", joke);
