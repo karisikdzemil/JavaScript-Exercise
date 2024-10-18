080ebb5f6bf29644e36b45ee6bcbd26427936fb7
dzemil karisik <karisikdzemil@gmail.com>
2024-10-18 17:53:48 +0200

    new project

--- drums/app.js
+++
@@ -1,8 +0,0 @@
-const btns = document.querySelectorAll("button");
-
-btns.forEach((el)=>{
-    el.addEventListener("click", (event)=>{
-        console.log(event.target.textContent);
-    })
-    console.log(el)
-})
