db2204cee4461c60554039d2b932be630267b4eb
dzemil karisik <karisikdzemil@gmail.com>
2024-10-25 18:17:09 +0200

    galery

--- responsive-menu-slider-portfolio-modal-main/script.js
+++ responsive-menu-slider-portfolio-modal-main/script.js
@@ -1,3 +1,4 @@
+// Ovo je responsive meni
 const mobileMenu = () => {
     let menu = document.querySelector(".header ul");
     let btn = document.querySelector(".header button");
@@ -13,3 +14,38 @@ const mobileMenu = () => {
     }
 
 }
+
+// Ovo je galerija
+
+let rightBtn = document.querySelector("#right-btn");
+let leftBtn = document.querySelector("#left-btn");
+let pictures = document.querySelectorAll(".slider-images img");
+
+let imgNum = 0;
+
+const moveRight = () => {
+    displayNone();
+    imgNum++;
+    if(imgNum === pictures.length){
+        imgNum = 0;
+    }
+    pictures[imgNum].style.display = "block";
+}
+const moveLeft = () => {
+    displayNone();
+    imgNum--;
+    if(imgNum === -1){
+        imgNum = pictures.length -1;
+    }
+    pictures[imgNum].style.display = "block";
+}
+
+rightBtn.addEventListener("click", moveRight)
+leftBtn.addEventListener("click", moveLeft);
+
+const displayNone = () =>{
+    pictures.forEach((img)=>{
+        img.style.display = "none";
+    })
+}
+
