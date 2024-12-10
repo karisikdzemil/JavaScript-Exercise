d528cf5875a186a22dd75357a834954bd9704859
dzemil karisik <karisikdzemil@gmail.com>
2024-12-10 18:03:34 +0100

    todo with oop is done

--- ToDo-OOP/app.js
+++ ToDo-OOP/app.js
@@ -1,13 +1,55 @@
 const input = document.querySelector('input');
 const inputBtn = document.getElementById('add-btn');
+const ul = document.querySelector("ul");
+const allButtons = document.getElementsByClassName("li-but");
+const allLi = document.querySelectorAll("li");
 
 class ToDo{
-    constructor(inputValue){
-        inputValue = this.inputValue;
+    
+    constructor(){
+        this.arr = [];
+        this.deleteTodo();
     }
 
     getValue(){
-        console.log(input.value);
+        if(input.value === ''){
+            alert("Please enter value!");
+        }else{
+            const value = input.value;
+            this.arr.push(value);
+            console.log(this.arr);
+        }
+        input.value = '';;
+        this.renderItems();
+        this.arr = [];
+        this.deleteTodo();
     }
+
+    renderItems(){
+        this.arr.map((el)=>{
+            const li = document.createElement("li");
+            const liBtn = document.createElement("button");
+            li.innerText = el;
+            liBtn.className = "li-but";
+            liBtn.innerText = "delete";
+            li.appendChild(liBtn);
+            ul.appendChild(li);
+        })
+    }
+    deleteTodo() {
+        for (const but of allButtons) {
+            but.addEventListener("click", () => {
+                const removedEl = but.closest("li"); 
+                if (removedEl) {
+                    removedEl.remove();
+                }
+            });
+        }
+    }
+    
 }
+let todo = new ToDo();
+inputBtn.addEventListener("click", todo.getValue.bind(todo));
+
+
 
