6e3effd182d60128929a18e7b8ffc8d41856749a
dzemil karisik <karisikdzemil@gmail.com>
2024-12-12 16:58:26 +0100

    finidhing project

--- Quiz-OOP/app.js
+++ Quiz-OOP/app.js
@@ -2,6 +2,12 @@ const question = document.querySelector("h2");
 const answers = document.querySelectorAll(".li");
 const next = document.getElementById("next");
 const ul = document.querySelector("ul");
+const counted = document.querySelector("h3");
+const blackDrop = document.querySelector(".black-drop");
+const modal = document.querySelector('.modal');
+let answered = false;
+let count = 0;
+let correctAnswers = 0
 
 
 class Quiz{
@@ -12,38 +18,45 @@ class Quiz{
     init(){
         this.show((Math.random() * 10).toFixed(0));
         next.addEventListener("click", () => proba.show((Math.random() * 10).toFixed(0)));
-
-        ul.addEventListener("click", (e)=>{
-            let targetEl;
-            if(e.target.tagName === "LI"){
-               for(const el of this.arr){
-                if(el.question === question.textContent){
-                    targetEl = el;
-                }
-               }
-
-               if(targetEl.cAnswer === e.target.textContent){
-                e.target.classList = 'true';
-               }else{
-                e.target.classList = 'false';
-               }
-
-            }
-        })
+        ul.addEventListener("click", e => {this.checkQuestion(e)});
     }
+
     show(randomNumber){
+        answered = false;
+        answers.forEach(el => el.className = 'li')
         question.innerText = this.arr[randomNumber].question;
         this.arr[randomNumber].wAnswers.forEach((el, i) => {
             answers[i].innerText = el;
         });
+        count++;
+        this.gameOverCounter();
     }
 
-    // answer(){
+    checkQuestion(e){
+        if(e.target.tagName !== "UL" && answered === false){
+            const targetEl = this.arr.find((el)=>{
+               return el.question === question.textContent;
+            })
 
-    // }
+            if(e.target.textContent === targetEl.cAnswer){
+                e.target.classList = 'true';
+                answered = true;
+                correctAnswers++;
+            }else{
+                e.target.classList = 'false';
+                answered = true;
+            }
+        }
+    }
+
+    gameOverCounter(){
+        counted.innerText = `${count} of 4`;
+        if(count > 4){
+            blackDrop.classList.add("visible");
+            modal.classList.add("visible");
+            count = 0;
+        }
+    }
+    
 }
-
 const proba = new Quiz(quiz);
-ul.addEventListener('click', (el)=>{
-
-});
