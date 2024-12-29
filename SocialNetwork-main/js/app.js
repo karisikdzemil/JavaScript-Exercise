12897ecce261837c842274315eb56741e80b0f58
dzemil karisik <karisikdzemil@gmail.com>
2024-12-29 20:25:36 +0100

    social network

--- SocialNetwork-main/js/app.js
+++ SocialNetwork-main/js/app.js
@@ -5,3 +5,42 @@ document.querySelector("#registracija").addEventListener("click", () => {
 document.querySelector("#closeModal").addEventListener("click", () => {
     document.querySelector(".custom-modal").style.display = 'none';
 })
+
+let config = {
+    "korisnicko_ime": {
+        required: true, 
+        minlength: 5,
+        maxlength: 50
+    },
+    "register_email": {
+        required: true, 
+        email: true,
+        minlength: 5,
+        maxlength: 50
+    },
+   
+    "register_lozinka": {
+        required: true,
+        minlength: 7,
+        maxlength: 25,
+        matching: 'ponovi_lozinku'
+    },
+    "ponovi_lozinku": {
+        required: true,
+        minlength: 7,
+        maxlength: 25,
+        matching: "register_lozinka"
+    }
+}
+
+let validator = new Validator(config, '#registrationForm');
+
+document.querySelector("#registrationForm").addEventListener("submit", e => {
+    e.preventDefault();
+
+    if(validator.validationPassed()){
+        alert("Sve je ok")
+    }else{
+        alert("Nije ok")
+    }
+})
