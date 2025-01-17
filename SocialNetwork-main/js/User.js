30f86a8b1db03022bf290188de5c798644320e59
dzemil karisik <karisikdzemil@gmail.com>
2025-01-17 21:03:37 +0100

    logic for

--- SocialNetwork-main/js/User.js
+++ SocialNetwork-main/js/User.js
@@ -27,6 +27,37 @@ class User{
         });
     }
 
+    async get(user_id){
+        let api_url = this.api_url + "/users/" + user_id;
+       let response = await fetch(api_url);
+       let data = await response.json();
+       return data;
+    }
+
+    edit(){
+        let data = {
+            username: this.username,
+            email: this.email
+        };
+
+        data = JSON.stringify(data);
+
+        let session = new Session();
+        session_id = session.getSession();
+
+        fetch(this.api_url + "/users/" + session_id, {
+            method: "PUT",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: data
+        })
+        .then(response => response.json())
+        .then(data => {
+            window.location.href = "hexa.html";
+        })
+    }
+
     login(){
         fetch(this.api_url + "/users")
         .then(response => response.json())
@@ -45,6 +76,22 @@ class User{
             if(login_successful === 0){
                 alert("pogresan email ili lozinka!!");
             }
+        })
+    }
+
+    delete(){
+        let session = new Session();
+        session_id = session.getSession();
+
+        fetch(this.api_url + "/users/" + session_id, {
+            method: "DELETE"
+        })
+        .then(response => response.json())
+        .then(data => {
+            let session = new Session();
+            session.destroySession();
+
+            window.location.href = "hexa.html";
         })
     }
 }
--- SocialNetwork-main/js/hexa.js
+++ SocialNetwork-main/js/hexa.js
@@ -1,8 +1,56 @@
 let session = new Session();
-session = session.getSession();
+session_id = session.getSession();
 
-if(session !== ""){
-    
+if(session_id !== ""){
+   
+
+    async function populateUserData(){
+        let user = new User();
+        user = await user.get(session_id);
+        document.querySelector("#username").innerText = user["username"];
+        document.querySelector("#email").innerText = user["email"];
+
+        document.querySelector("#korisnicko_ime").value = user["username"];
+        document.querySelector("#edit_email").value = user["email"];
+
+    }
+    populateUserData();
 }else{
     window.location.href = "/";
 }
+
+document.querySelector("#logout").addEventListener("click", e => {
+    e.preventDefault();
+
+    session.destroySession();
+    window.location.href = '/';
+})
+
+document.querySelector("#editAccount").addEventListener("click", () => {
+    document.querySelector(".custom-modal").style.display = 'block';
+});
+
+document.querySelector("#closeModal").addEventListener("click", () => {
+    document.querySelector(".custom-modal").style.display = 'none';
+})
+
+document.querySelector("#editForm").addEventListener("submit", e => {
+    e.preventDefault();
+
+    let user = new User();
+    user.username = document.querySelector("#korisnicko_ime").value;
+    user.email = document.querySelector("#edit_email").value;
+    user.edit();
+});
+
+document.querySelector("#deleteProfile").addEventListener("click", e => {
+    e.preventDefault();
+
+    let text = "Da li ste sigurni da zelite da obrisete profil?";
+
+    if(confirm(text) === true){
+        let user = new User(session_id);
+        user.delete();
+
+    }
+})
