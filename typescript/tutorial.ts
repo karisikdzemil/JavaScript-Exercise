ddc64493f4f5227a099c521b6127b28172d90027
dzemil karisik <karisikdzemil@gmail.com>
2025-06-15 16:45:39 +0200

    type annotations

---
+++ typescript/tutorial.ts
@@ -0,0 +1,7 @@
+let id: number = 5;
+let company: string = "Acme Corp";
+let isPublished: boolean = true;
+
+let ids: number[] = [1, 2, 3, 4, 5];
+let x: any = "pedro";
+let xArr:any = ["pedro", 0, true];
