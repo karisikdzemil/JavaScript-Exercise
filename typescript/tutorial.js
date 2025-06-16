e4c8b8750070e140eb802fef9ab11505affdbcb5
dzemil karisik <karisikdzemil@gmail.com>
2025-06-16 14:10:18 +0200

    interfaces and types

---
+++ typescript/tutorial.js
@@ -0,0 +1,17 @@
+"use strict";
+// let id: number = 5;
+// let company: string = "Acme Corp";
+// let isPublished: boolean = true;
+const User = {
+    id: 2,
+    name: "Pedro",
+    greet(message) {
+        console.log(message);
+    }
+};
+User.greet("Hello");
+// if(!User.age){
+//     console.log("No age Of the User!");
+// }else{
+//     console.log(User.age);
+// }
--- typescript/tutorial.ts
+++ typescript/tutorial.ts
@@ -1,7 +1,43 @@
-let id: number = 5;
-let company: string = "Acme Corp";
-let isPublished: boolean = true;
+// let id: number = 5;
+// let company: string = "Acme Corp";
+// let isPublished: boolean = true;
 
-let ids: number[] = [1, 2, 3, 4, 5];
-let x: any = "pedro";
-let xArr:any = ["pedro", 0, true];
+// let ids: number[] = [1, 2, 3, 4, 5];
+// let x: any = "pedro";
+// let xArr:any = ["pedro", 0, true];
+
+// const concatValues = (a: string, b:string): string => {
+//     return a + b;
+// }
+
+// console.log(concatValues("Hello ", "World"));
+// console.log(concatValues("5", "10"));
+
+// user infterfaces
+
+// interface UserInterface {
+//     id: number,
+//     name: string,
+//     age?: number,
+//     greet(message: string): void
+// }
+
+// const User: UserInterface = {
+//     id: 2,
+//     name: "Pedro",
+//     greet(message){
+//         console.log(message)
+//     }
+// }
+
+// User.greet("Hello");
+
+// if(!User.age){
+//     console.log("No age Of the User!");
+// }else{
+//     console.log(User.age);
+// }
+
+const printID = (id) => {
+    console.log("ID: " +  id)
+}
